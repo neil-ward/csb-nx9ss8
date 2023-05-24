30b42b81541d0808e12708db75c268862dc2fcc8
Code Sandbox <hello@codesandbox.io>
2023-05-24 19:56:26 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.836ac36c204433aa6ccffc6df2363fdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23644ed16b8f155107fa9102bd3d189f",
+    "url": "https://.github.io/csb-nx9ss8/index.html"
+  },
+  {
+    "revision": "9fb07095ccecd55e11d0",
+    "url": "https://.github.io/csb-nx9ss8/static/css/main.5e1d73c1.chunk.css"
+  },
+  {
+    "revision": "11e8a704fc92d9c65a53",
+    "url": "https://.github.io/csb-nx9ss8/static/js/2.eb853afb.chunk.js"
+  },
+  {
+    "revision": "55d2529880086d75c9e3aeb1360db402",
+    "url": "https://.github.io/csb-nx9ss8/static/js/2.eb853afb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fb07095ccecd55e11d0",
+    "url": "https://.github.io/csb-nx9ss8/static/js/main.48d1fde0.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "https://.github.io/csb-nx9ss8/static/js/main.48d1fde0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfda7b81be7563b60136",
+    "url": "https://.github.io/csb-nx9ss8/static/js/runtime-main.7dc3c558.js"
+  }
+]);
